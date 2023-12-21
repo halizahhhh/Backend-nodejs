dd27a22b359fe5508e96873a0966ec13161aa5f2
halizahhhh <halizahhhh@gmail.com>
2023-12-21 21:08:46 +0700

    Praktikum 11

---
+++ Praktikum11/task/data/students.js
@@ -0,0 +1,7 @@
+// TODO 1: Buat data students
+// code here
+let students = ["Siti Nurhalizah", "Vikri Putra Permana", "Muhammad Furqon Rizqi"];
+
+// TODO 2: export data students
+// code here
+module.exports = students;
